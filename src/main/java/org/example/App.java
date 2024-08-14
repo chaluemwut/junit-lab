b0d23b7add689db17462e75a877edf040b372177
chaluemwut <chaluemwut@users.noreply.github.com>
2024-08-14 04:49:25 +0000

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,11 @@ package org.example;
  */
 public class App 
 {
+
+    public int getSum(int a, int b){
+        return a+b;
+    }
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -33,6 +33,8 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+        App app = new App();
+        int sum = app.getSum(1,2);
+        assertTrue( sum == 3);
     }
 }
