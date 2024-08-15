076b6a4427c1f06b2b5999fdd89d9b030b73d97f
chaluemwut <chaluemwut.dev@myorder.ai>
2024-08-15 16:43:53 +0700

    add junit

--- src/test/java/org/example/TestMethod.java
+++ src/test/java/org/example/TestMethod.java
@@ -1,8 +1,9 @@
 package org.example;
 
 
-import junit.framework.Assert;
+
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMethod {
@@ -12,5 +13,12 @@ public class TestMethod {
         App app = new App();
         int sum = app.getSum(1, 2);
         Assert.assertTrue(sum == 3);
+    }
+
+    @Test
+    public void testNotCorrect(){
+        App app = new App();
+        int sum = app.getSum(2, 2);
+        Assert.assertTrue(sum==5);
     }
 }
